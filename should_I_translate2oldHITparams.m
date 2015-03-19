462f6f56cdf58f9758bacfbf5263f952f71fdd97
Sergio Machado <sergio@umbc.edu>
2015-03-19 15:19:11 -0400

    SSM : enough .m files for run8.m to work

---
+++ should_I_translate2oldHITparams.m
@@ -0,0 +1,16 @@
+function lineNEW = should_I_translate2oldHITparams(line,gasID,hitran_version);
+
+lineNEW = line;
+
+bwah = 0;
+if strfind(hitran_version,'h04')
+  bwah = 1;
+elseif strfind(hitran_version,'h08')
+   bwah = 1;
+elseif strfind(hitran_version,'h12') 
+  bwah = 1; 
+end
+if bwah > 0 & gasID == 2
+  disp('for CO2, need to translate H04,H08,H12 char(ilsgq,iusgq) to integer')
+  lineNEW = translate2oldHITparams(line);
+end
